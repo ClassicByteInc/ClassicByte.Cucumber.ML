582338e2d682ba177220dae4c9f301d0d1f2c0dc
Shimikoi <shimikoiku@outlook.com>
2024-06-10 13:36:26 +0800

    添加项目文件。

---
+++ ClassicByte.Cucumber.ML.AI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using ClassicByte_Cucumber_ML_AI;
+using ClassicByte_Cucumber_ML_Test;
+
+namespace ClassicByte.Cucumber.ML.AI
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    var input = new PictureLearnModel.ModelInput();
+                    Console.Write("输入你的照片的位置:");
+                    var imgpath = Console.ReadLine();
+
+                    var web = new WebClient();
+                    var data = web.DownloadData(imgpath);
+                    input.ImageSource = data;
+
+                    var result = PictureLearnModel.Predict(input);
+                    switch (result.PredictedLabel)
+                    {
+                        case "初音未来":
+                            Console.WriteLine("这是初音未来吧");
+                            break;
+                        case "洛天依":
+                            Console.WriteLine("这是洛天依吧");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"\n{e.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ ClassicByte.Cucumber.ML.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassicByte_Cucumber_ML_Test;
+
+namespace ClassicByte.Cucumber.ML.Test
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = new PictureLearnModel.ModelInput();
+            Console.WriteLine();
+            input.ImageSource = File.ReadAllBytes();
+        }
+    }
+}
